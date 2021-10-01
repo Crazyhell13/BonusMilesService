8d3e769d1105a9e8fb8cf0e293d2d54e7fd5264e
crazyhell13 <crazyhell13@gmail.com>
2021-10-01 22:59:00 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int bonusMile = 2000;
+        int miles = cost / bonusMile;
+        return miles;
+    }
+
+}
+
+
+
